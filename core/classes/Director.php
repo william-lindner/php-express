1bfa28785bbc9df354dc1104ce41a9ecda9892ef
william-lindner <wlindner@apple.com>
2018-11-07 09:59:16 -0500

    adding teapot namespace to core class structure

--- core/classes/Director.php
+++ core/classes/Director.php
@@ -1,43 +1,49 @@
 <?php
-class Director {
- private $controller;
- private $action;
 
- public function __construct($http = '') {
+namespace Teapot;
 
-  // This splits the GET request into / pieces
-  $request = explode('/', $http['url']);
-  array_pop($request);
-  var_dump($request);
+class Director
+{
+    private $controller;
+    private $action;
 
-  // This ensures that if you are on the root directory the user
-  // is routed to the index.
-  if (empty($request)) {
-   $this->controller = 'Home';
-   $this->action     = 'index';
-   return;
-  }
+    public function __construct($http = '')
+    {
 
-  $this->controller = $request[0];
-  $this->action     = 'index';
+        // This splits the GET request into / pieces
+        $request = explode('/', $http['url']);
+        array_pop($request);
+        var_dump($request);
 
- }
+        // This ensures that if you are on the root directory the user
+        // is routed to the index.
+        if (empty($request)) {
+            $this->controller = 'Home';
+            $this->action     = 'index';
+            return;
+        }
 
- public function createController() {
+        $this->controller = $request[0];
+        $this->action     = 'index';
 
-  if (class_exists($this->controller)) {
-   $parents = class_parents($this->controller);
-   if (in_array("Controller", $parents)) {
-    if (method_exists($this->controller, $this->action)) {
-     return new $this->controller($this->action, $this->request);
     }
-   } else {
-    echo 'does not exist';
-    return;
-   }
-  } else {
-   echo 'not found';
-   return;
-  }
- }
+
+    public function createController()
+    {
+
+        if (class_exists($this->controller)) {
+            $parents = class_parents($this->controller);
+            if (in_array("Controller", $parents)) {
+                if (method_exists($this->controller, $this->action)) {
+                    return new $this->controller($this->action, $this->request);
+                }
+            } else {
+                echo 'does not exist';
+                return;
+            }
+        } else {
+            echo 'not found';
+            return;
+        }
+    }
 }
--- core/classes/Model.php
+++ core/classes/Model.php
@@ -1,103 +1,82 @@
 <?php
-/*
 
-@Name: MTS Model Class
-@Author: William Lindner
-@Version: 1.5
-@License: MIT
+namespace Teapot;
 
-Copyright (c) 2018 william-lindner
+abstract class Model
+{
+    protected $db;
+    protected $request;
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+    protected $status = 'unknown';
+    protected $reason = 'unknown';
+    protected $state  = 501; # https://www.w3schools.com/tags/ref_httpmessages.asp
 
------------------------------------------------------
+    private $dataType = 'json';
+    private $data;
 
- */
+    public function __construct($dataType = 'json')
+    {
+        // setup the database connection
+        $this->db = new db_connect();
+    }
+    public function request($request = null, $data = [])
+    {
 
-abstract class Model {
- protected $db;
- protected $request;
+        // check to see if a request is being made
+        if (!$request || !is_string($request)) {
+            if (is_array($request)) {
+                if (isset($request['request'])) {
+                    // when the request is an array, attempt to interpret the array
+                    $data    = $request;
+                    $request = $request['request'];
+                    unset($data['request']);
+                }
 
- protected $status = 'unknown';
- protected $reason = 'unknown';
- protected $state  = 501; # https://www.w3schools.com/tags/ref_httpmessages.asp
+            } else {
+                $this->returnError('No request passed to the model.');
+            }
+        }
+        $this->request = $request;
+        // check for the method and return error if not found
+        if (!method_exists($this, $request)) {
+            $this->returnError('Request method does not exist.');
+        }
 
- private $dataType = 'json';
- private $data;
+        // load the request method
+        $results = $this->{$this->request}($data);
+        if ($results !== null) {
+            $this->status = 'success';
+            $this->reason = 'All data acquired successfully.';
+            $this->state  = 200;
+            $this->echoResults($results);
+        } else {
+            $this->echoResults('No results returned from Model', 204);
+        }
 
- public function __construct($dataType = 'json') {
-  // setup the database connection
-  $this->db = new db_connect();
- }
- public function request($request = null, $data = []) {
-
-  // check to see if a request is being made
-  if (!$request || !is_string($request)) {
-   if (is_array($request)) {
-    if (isset($request['request'])) {
-     // when the request is an array, attempt to interpret the array
-     $data    = $request;
-     $request = $request['request'];
-     unset($data['request']);
     }
 
-   } else {
-    $this->returnError('No request passed to the model.');
-   }
-  }
-  $this->request = $request;
-  // check for the method and return error if not found
-  if (!method_exists($this, $request)) {
-   $this->returnError('Request method does not exist.');
-  }
+    protected function returnError($reason, $state = 501)
+    {
+        // set the status to an error state
+        $this->status = 'error';
+        $this->reason = $reason;
+        $this->state  = $state;
+        $this->echoResults();
+    }
 
-  // load the request method
-  $results = $this->{$this->request}($data);
-  if ($results !== null) {
-   $this->status = 'success';
-   $this->reason = 'All data acquired successfully.';
-   $this->state  = 200;
-   $this->echoResults($results);
-  } else {
-   $this->echoResults('No results returned from Model', 204);
-  }
-
- }
-
- protected function returnError($reason, $state = 501) {
-  // set the status to an error state
-  $this->status = 'error';
-  $this->reason = $reason;
-  $this->state  = $state;
-  $this->echoResults();
- }
-
- protected function echoResults($results = [], $dataType = 'json') {
-  // fix the results to an array if not array by default
-  switch ($dataType) {
-  default:
-   echo json_encode([
-    'status'  => $this->status,
-    'reason'  => $this->reason,
-    'state'   => $this->state,
-    'results' => $results,
-   ]);
-  }
-  exit;
- }
+    protected function echoResults($results = [], $dataType = 'json')
+    {
+        // fix the results to an array if not array by default
+        switch ($dataType) {
+            default:
+                echo json_encode([
+                    'status'  => $this->status,
+                    'reason'  => $this->reason,
+                    'state'   => $this->state,
+                    'results' => $results,
+                ]);
+        }
+        exit;
+    }
 
 }
