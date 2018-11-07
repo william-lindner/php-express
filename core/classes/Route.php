620b8cb1a89bc29778f347a59587da0c195c9343
william-lindner <wlindner@apple.com>
2018-11-07 10:44:03 -0500

    adding routes file

--- core/classes/Route.php
+++ core/classes/Route.php
@@ -4,5 +4,18 @@ namespace Teapot;
 
 class Route
 {
+    public static function get($path, Callback $callback)
+    {
 
+    }
+
+    public static function post($path, Callback $callback)
+    {
+
+    }
+
+    public static function patch($path, Callback $callback)
+    {
+
+    }
 }
