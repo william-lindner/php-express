5b8f232229020f81bf3dc059be0f070213862be9
william-lindner <wlindner@apple.com>
2018-11-07 11:09:58 -0500

    fixing autoloader variable notice

--- core/setup.php
+++ core/setup.php
@@ -2,3 +2,8 @@
 
 require __DIR__ . '/utilities/helpers.php';
 require __DIR__ . '/utilities/autoload.php';
+require __DIR__ . '/classes/Teapot.php';
+
+use Teapot\Teapot;
+
+new Teapot;
--- core/utilities/autoload.php
+++ core/utilities/autoload.php
@@ -4,8 +4,9 @@ spl_autoload_register(function ($class) {
     if (strpos($class, 'Teapot') === false) {
         return;
     }
-    $class = array_pop(explode('\\', $class));
-    $path  = __DIR__ . "/../classes/{$class}.php";
+    $request = explode('\\', $class);
+    $class   = array_pop($request);
+    $path    = __DIR__ . "/../classes/{$class}.php";
     if (file_exists($path)) {
         require_once $path;
     }
