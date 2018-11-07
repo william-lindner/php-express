7d162c7bb7e33e6f6f390ed16ed73873b01220c0
william-lindner <wlindner@apple.com>
2018-11-07 10:48:21 -0500

    removing error checking on callable core helpers

--- core/utilities/helpers.php
+++ core/utilities/helpers.php
@@ -1,45 +1,36 @@
 <?php
-if (!is_callable('dump')) {
-
-    /**
-     * Creates an output string to be echoed.
-     * @param any $content
-     * @return string
-     */
-    function dump($content)
-    {
-        ob_start();
-        echo '<pre>';
-        var_dump($content);
-        echo '</pre>';
-        return ob_get_clean();
-    }
+/**
+ * Creates an output string to be echoed.
+ * @param any $content
+ * @return string
+ */
+function dump($content)
+{
+    ob_start();
+    echo '<pre>';
+    var_dump($content);
+    echo '</pre>';
+    return ob_get_clean();
 }
 
-if (!is_callable('dd')) {
-
-    /**
-     * Uses the dump output string to exit execution and display dumped content.
-     * @param any $content
-     * @return void
-     */
-    function dd($content)
-    {
-        die(dump($content));
-    }
+/**
+ * Uses the dump output string to exit execution and display dumped content.
+ * @param any $content
+ * @return void
+ */
+function dd($content)
+{
+    die(dump($content));
 }
 
-if (!is_callable('nprint')) {
-
-    /**
-     * Prints the output and adds a new line after.
-     * @param string $output
-     * @return void
-     */
-    function nprint($output)
-    {
-        echo $output . '<br>';
-    }
+/**
+ * Prints the output and adds a new line after.
+ * @param string $output
+ * @return void
+ */
+function nprint($output)
+{
+    echo $output . '<br>';
 }
 
 if (!is_callable('config')) {
