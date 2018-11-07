685c8b05b90d67b4d80c28da948fba259ca5c32a
william-lindner <wlindner@apple.com>
2018-11-07 12:37:40 -0500

    removed class_exists as it caused errors with namespace repitition

--- core/utilities/autoload.php
+++ core/utilities/autoload.php
@@ -6,9 +6,6 @@ spl_autoload_register(function ($class) {
     }
     $request = explode('\\', $class);
     $class   = array_pop($request);
-    if (class_exists($class)) {
-        return;
-    }
 
     $path = __DIR__ . "/../classes/{$class}.php";
     if (file_exists($path)) {
