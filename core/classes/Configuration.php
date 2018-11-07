60e115539a9c5c7fddd0eb4cf42e65044c12fb79
william-lindner <wlindner@apple.com>
2018-11-07 11:31:01 -0500

    basic exception handler in place

--- core/classes/Configuration.php
+++ core/classes/Configuration.php
@@ -6,6 +6,7 @@ class Configuration
 {
     public static function load()
     {
+        // TODO: Add in try catch
         return parse_ini_file($_SERVER['DOCUMENT_ROOT'] . '/../.ini', true);
     }
 }
--- core/classes/Exception.php
+++ core/classes/Exception.php
@@ -4,8 +4,19 @@ namespace Teapot;
 
 class Exception
 {
+    /**
+     * Registers the Exception handler within the class to output all exceptions in Teapot format.
+     * @return void
+     */
     public static function register()
     {
+        set_exception_handler(['\Teapot\Exception', 'handler']);
+    }
 
+    public static function handler($e)
+    {
+        $trace = debug_backtrace();
+        echo dump($e);
+        return false;
     }
 }
