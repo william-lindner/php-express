78b0ec228aa029a0b86f092bbd03fc33a0230088
william-lindner <wlindner@apple.com>
2018-11-09 19:35:41 -0500

    adding autoload from composer

--- core/setup.php
+++ core/setup.php
@@ -3,7 +3,7 @@
 use Teapot\Teapot;
 
 require __DIR__ . '/utilities/helpers.php';
-require __DIR__ . '/utilities/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 Teapot::fill();
 Teapot::boil();
