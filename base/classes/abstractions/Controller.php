595f981731d4b61d6cfedf6783c3484227d373b7
william-lindner <wlindner@apple.com>
2019-04-08 20:59:29 -0400

    Moving Controller resource functions away from Model

--- base/classes/abstractions/Controller.php
+++ base/classes/abstractions/Controller.php
@@ -11,4 +11,41 @@ abstract class Controller
         $this->request = $request;
     }
 
+    protected function httpError($reason = false, $code = 418)
+    {
+
+        $protocol = $_SERVER["SERVER_PROTOCOL"] ?? 'HTTP/1.1';
+        $reason   = $reason ?: 'No Tea';
+        $code     = (int) $code;
+
+        ob_clean();
+        header("$protocol $code $reason", true, $code);
+        http_response_code($code);
+    }
+
+    /**
+     * Allows for JSON object echo within APIs
+     *
+     */
+    protected function jsonify($contents)
+    {
+        ob_clean();
+        header('Content-Type: application/json;charset=utf-8');
+        exit(json_encode($contents));
+    }
+
+    /**
+     * Requests an internal function from this
+     */
+    public function request($data)
+    {
+        $method = is_array($data) ? $data['request'] : $data;
+
+        if (!method_exists($this, $method)) {
+            throw new \Exception('Method requested does not exist in ' . get_class($this), 400);
+        }
+
+        $this->$method();
+    }
+
 }
