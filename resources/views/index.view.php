27da0d8120d16624becccaa086ba52a71952d86d
william-lindner <wlindner@apple.com>
2019-04-08 20:54:38 -0400

    Adding cosmetic look and feel to index

--- resources/views/index.view.php
+++ resources/views/index.view.php
@@ -2,6 +2,8 @@
 <?php include __DIR__ . '/layout/head.view.php'?>
 
 <main class='express-wrapper'>
+    <div class="express-wrapper__background">
+    </div>
     <img class='express-wrapper__logo' src="/img/logo.png" alt="">
 </main>
 
