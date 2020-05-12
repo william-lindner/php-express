a6f323aac96236e245c55306ec29748886bf4b07
William Lindner <wlindner@bizjournals.com>
2020-05-12 10:19:40 -0400

    init

--- base/classes/abstractions/Controller.php
+++ base/classes/abstractions/Controller.php
@@ -4,27 +4,5 @@ namespace Express\Abstractions;
 
 abstract class Controller
 {
-    protected function httpError(?string $reason = null, int $code = 400)
-    {
-        $protocol = $_SERVER['SERVER_PROTOCOL'] ?? 'HTTP/1.1';
-        $reason = (string) $reason ?: 'Unknown Error';
-
-        ob_clean();
-        header("$protocol $code $reason", true, $code);
-        http_response_code($code);
-    }
-
-    /**
-     * Allows for JSON object echo within APIs
-     *
-     * @param $contents
-     *
-     * @return false|string
-     */
-    protected function jsonify($contents)
-    {
-        ob_clean();
-        header('Content-Type: application/json;charset=utf-8');
-        return json_encode($contents);
-    }
+    //
 }
--- base/classes/http/Response.php
+++ base/classes/http/Response.php
@@ -4,17 +4,54 @@ namespace Express\Http;
 
 class Response
 {
-    const CONTENT_TYPES = [
-        'json' => 'application/json',
+
+    public const CONTENT_HTML = 'html';
+
+    protected const DEFAULT_TYPE = 'html';
+
+    private const CONTENT_TYPES = [
+        'json'      => 'application/json',
+        'text'      => 'text/html',
+        'html'      => 'text/html',
+        'form-data' => 'multipart/form-data',
+        'xml'       => 'application/xml'
     ];
 
     public $charset;
 
+    public $contentType;
+
+    public $output = '';
+
+    private $buffering = false;
+
     public function __construct(?string $contents = null)
     {
-        ob_start();
-
+        $this->contentType = self::CONTENT_TYPES[static::DEFAULT_TYPE];
         $this->charset = 'utf-8';
+
+        if ($contents === null) {
+            $this->output = $contents;
+
+
+        } else {
+            $this->buffering = ob_start();
+        }
+
+    }
+
+    public function code(int $code = 200)
+    {
+        http_response_code($code);
+        return $this;
+    }
+
+    public function json(array $data = [])
+    {
+        $this->charset = 'utf-8';
+        $this->header('json');
+
+        $this->output = json_encode($data);
     }
 
     public function header(string $type)
@@ -26,14 +63,25 @@ class Response
         return $this;
     }
 
-    public function code(int $code = 200)
+    public function error(string $reason = '', int $code = 400)
     {
+        $protocol = $_SERVER['SERVER_PROTOCOL'] ?? 'HTTP/1.1';
+        $reason = $reason ?: 'Unknown Error';
+
+        header("$protocol $code $reason", true, $code);
         http_response_code($code);
-        return $this;
     }
 
-    public function send()
+
+    /**
+     *
+     */
+    public function send() : void
     {
-        echo ob_get_clean();
+        if ($this->buffering) {
+            $this->output = ob_get_clean();
+        }
+
+        echo $this->output;
     }
 }
